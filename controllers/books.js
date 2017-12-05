a1c41caa94339a1002ae5bf9eac0897c73fc3247
David Basil <info@basildavid.com>
2017-12-05 06:47:05 +0400

    add book details view and controller

--- controllers/books.js
+++ controllers/books.js
@@ -1,7 +1,18 @@
 var myApp = angular.module('myApp')
 
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
+
 	$scope.getBooks = function(){
-		$http.get()
+		$http.get('http://localhost:3000/books').then(function(response){
+			$scope.books = response.data
+		})
 	}
+
+	$scope.getBook = function(){
+		var id = $routeParams.id
+		$http.get('http://localhost:3000/books/' + id).then(function(response){
+			$scope.book = response.data
+		})
+	}
+
 }])
