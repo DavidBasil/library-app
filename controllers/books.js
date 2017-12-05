fc8ab4de7a44e743daa0153d66fbfce4a417173a
David Basil <info@basildavid.com>
2017-12-05 10:29:21 +0400

    add package.json

--- controllers/books.js
+++ controllers/books.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp')
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 
 	$scope.getBooks = function(){
-		$http.get('http://localhost:3000/books').then(function(response){
+		$http.get('mongodb://dbasil:Meanjs100!@ds129946.mlab.com:29946/library-app').then(function(response){
 			$scope.books = response.data
 		})
 	}
