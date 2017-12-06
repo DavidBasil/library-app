b000d7eda5ff2000d6a335eac727ee2c6c766b21
David Basil <info@basildavid.com>
2017-12-06 09:10:08 +0400

    remove firebase and add our own api

--- controllers/books.js
+++ controllers/books.js
@@ -3,33 +3,33 @@ var myApp = angular.module('myApp')
 myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 
 	$scope.getBooks = function(){
-		$http.get('mongodb://dbasil:Meanjs100!@ds129946.mlab.com:29946/library-app').then(function(response){
+		$http.get('https://mighty-plains-33079.herokuapp.com/api/books').then(function(response){
 			$scope.books = response.data
 		})
 	}
 
 	$scope.getBook = function(){
 		var id = $routeParams.id
-		$http.get('http://localhost:3000/books/' + id).then(function(response){
+		$http.get('https://mighty-plains-33079.herokuapp.com/api/books/' + id).then(function(response){
 			$scope.book = response.data
 		})
 	}
 
 	$scope.addBook = function(){
-		$http.post('http://localhost:3000/books/', $scope.book).then(function(response){
+		$http.post('https://mighty-plains-33079.herokuapp.com/api/books/', $scope.book).then(function(response){
 			window.location.href = '#!/books'
 		})
 	}
 
 	$scope.editBook = function(){
 		var id = $routeParams.id
-		$http.put('http://localhost:3000/books/' + id, $scope.book).then(function(response){
+		$http.put('https://mighty-plains-33079.herokuapp.com/api/books/' + id, $scope.book).then(function(response){
 			window.location.href = "#!/books"	
 		})
 	}
 
 	$scope.removeBook = function(id){
-		$http.delete('http://localhost:3000/books/' + id).then(function(response){
+		$http.delete('https://mighty-plains-33079.herokuapp.com/api/books/' + id).then(function(response){
 			window.location.href = '#!/books'
 		})
 	}
